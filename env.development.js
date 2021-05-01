10b3b27c0287254d9a808c018a0f1c814dc18a75
crest89 <bshuge02@gmail.com>
2021-05-02 01:48:31 +0900

    feat: env.setteng

---
+++ env.development.js
@@ -0,0 +1,6 @@
+module.exports = {
+  BASE_URL: 'http://localhost:3000',
+  CTFL_SPACE: '8m927c5jh0h3',
+  CTFL_ACCESS_TOKEN: 'DFxW4VcMotOHsoONOXfYRTBVpza3SUq_OKY9jDBQLbA',
+  CTFL_CONTENT_TYPE_POST: 'post'
+}
